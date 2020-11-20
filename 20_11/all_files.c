a8d6b598bf41e0f46230288f62ef5835be8180b6
Riernish <57988144+Riernish@users.noreply.github.com>
2020-11-20 10:08:02 +0300

    Update all_files.c

--- 20_11/all_files.c
+++ 20_11/all_files.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -9,20 +8,21 @@
 
 int main(int argc, char** argv) {
 
-        for (int i = 1; i < argc; i++) {
+        
 
-                DIR* target = opendir(argv[i]);
+                DIR* target = opendir(argv[1]);
                 if (target == NULL) {
 
                         fprintf(stderr, "%s", strerror(errno));
-                        continue;
+               
                 }
 
 
                 struct dirent* files;
                 while ((files = readdir(target)) != NULL )
                         printf("%s\n", files -> d_name);
-        }
+                closedir(target);
+        //}
 
         return 0;
 
