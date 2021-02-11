985f95a483ccd4aadf7ac47264321d6ee62cdc87
Riernish <57988144+Riernish@users.noreply.github.com>
2021-02-11 17:08:17 +0300

    Create my_server.h

---
+++ 11.02/my_server.h
@@ -0,0 +1,15 @@
+#ifndef _myserver_h
+#define _myserver_h
+
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define PATH "/tmp/mysock"
+#define BUFSZ 256
+
+#endif
