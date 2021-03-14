92bbebce6a7ced0f023e628f8e9e4418a1ff4539
Riernish <57988144+Riernish@users.noreply.github.com>
2021-03-14 13:26:58 +0300

    Update server.c

--- 11.02/server.c
+++ 11.02/server.c
@@ -1,83 +1,91 @@
+#include "my_server.h"
+
 int main() {
-        int sk, ret, ret_2;
-        struct sockaddr_un name = {0};
+    int sk, ret, ret_2;
+    struct sockaddr_in name = {0};
+    char ip_adr[20] = {0};
+    strncpy(ip_adr, MY_ADDRESS, sizeof(MY_ADDRESS));
 
-        sk = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (sk < 0) {
-                perror("Unable to create socket");
-                return 1;
-        }
+    sk = socket(AF_INET, SOCK_STREAM, 0);
+    if (sk < 0) {
+        perror("Unable to create socket");
+        return 1;
+    }
 
-        name.sun_family = AF_UNIX;
-        strncpy(name.sun_path, PATH, sizeof(PATH));
+    name.sin_family = AF_INET;
+    name.sin_port = htons(23456);
+    name.sin_addr.s_addr = inet_addr(ip_adr);
 
-        ret = bind(sk, (struct sockaddr*)&name, sizeof(name));
-        if (ret < 0) {
-                perror("Unable to create socket");
-                close(sk);
-                return 1;
-        }
+    ret = bind(sk, (struct sockaddr*)&name, sizeof(name));
+    if (ret < 0) {
+        perror("Unable to create socket");
+        close(sk);
+        return 1;
+    }
 
-        ret = listen(sk, 20);
-        if (ret){
-                perror("Unable to listen");
-                close(sk);
-                return 1;
-        }
+    ret = listen(sk, 20);
+    if (ret){
+        perror("Unable to listen");
+        close(sk);
+        return 1;
+    }
+
+
+
 
         while(1) {
-                int client_sk;
-                char buffer[BUFSZ] = {0};
+            int client_sk;
+            char buffer[BUFSZ] = {0};
 
-                client_sk = accept (sk, NULL, NULL);
-                if (client_sk < 0) {
-                        perror("Unable to accept");
-                                               exit(1);
-                }
+            client_sk = accept (sk, NULL, NULL);
+            if (client_sk < 0) {
+                perror("Unable to accept");
+                exit(1);
+            }
 
-                ret = read(client_sk, buffer, BUFSZ);
-                buffer[ret - 1] = '\0';
+            ret = read(client_sk, buffer, BUFSZ);
+            buffer[ret - 1] = '\0';
 
-                if (ret < 0 || ret >= BUFSZ) {
-                        printf("Unexpected red error or overflow %d\n", ret);
-                        break;
+            if (ret < 0 || ret >= BUFSZ) {
+                printf("Unexpected red error or overflow %d\n", ret);
+                break;
 
-                }
-                if (strcmp(buffer, "EXIT") == 0) {
-
-                        close(client_sk);
-                        break;
-                } else if (strcmp(buffer, "PRINT") == 0) {
-
-                        ret_2 = ret + read(client_sk, buffer, BUFSZ);
-                        buffer[ret_2 - 1] = '\0';
-                        if (ret_2 < 0 || ret_2 >= BUFSZ) {
-                                printf("Unexpected red error or overflow %d\n", ret);
-                                buffer[0] = '\0';
-
-                                break;
-
-                        }
-                        printf("%s", buffer);
-                        buffer[0] = '\0';
-                        close(client_sk);
-                } else {
-
-                        printf("Unknown command: %s", buffer);
-                        buffer[0] = '\0';
-                        close(client_sk);
-
-                }
-                
-
-                printf("%s\n", buffer);
-                buffer[0] = '\0';
+            }
+            if (strcmp(buffer, "EXIT") == 0) {
 
                 close(client_sk);
+                break;
+            } else if (strcmp(buffer, "PRINT") == 0) {
+
+                ret_2 = ret + read(client_sk, buffer, BUFSZ);
+                buffer[ret_2 - 1] = '\0';
+                if (ret_2 < 0 || ret_2 >= BUFSZ) {
+                    printf("Unexpected red error or overflow %d\n", ret);
+                    buffer[0] = '\0';
+
+                    break;
+
+                }
+                printf("%s", buffer);
+                buffer[0] = '\0';
+                close(client_sk);
+            } else {
+
+                printf("Unknown command: %s", buffer);
+                buffer[0] = '\0';
+                close(client_sk);
+
+            }
+
+
+
+            printf("%s\n", buffer);
+            buffer[0] = '\0';
+
+            close(client_sk);
         }
 
-        unlink (PATH);
+
+
         return 0;
 }
-
-
