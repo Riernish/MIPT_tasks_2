5e950450e6f041e53b94b55e9bd35473a4444878
Riernish <57988144+Riernish@users.noreply.github.com>
2021-03-14 13:27:25 +0300

    Update my_server.h

--- 11.02/my_server.h
+++ 11.02/my_server.h
@@ -8,8 +8,13 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
 
 #define PATH "/tmp/mysock"
 #define BUFSZ 256
+#define MY_ADDRESS "127.0.0.1"
 
 #endif
