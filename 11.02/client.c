c752f1842eb6016d14da8a86623869260fc9cdd1
Riernish <57988144+Riernish@users.noreply.github.com>
2021-03-14 13:26:20 +0300

    Update client.c

--- 11.02/client.c
+++ 11.02/client.c
@@ -2,55 +2,56 @@
 
 #define DUMMY_STR "London is the capital of Great Britain\n"
 
-int main() {
+int main(int argc, char** argv) {
 
-        int sk, ret;
-        struct sockaddr_un name = {0};
+    if (argc < 2) {
 
-        sk = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (sk < 0) {
-                perror("Unable to create socket");
-                exit(1);
-        }
-
-        name.sun_family = AF_UNIX;
-        strncpy(name.sun_path, PATH, sizeof(PATH));
-
-        char buffer[BUFSZ-1] = {0};
-        ret = read (STDIN_FILENO, buffer, BUFSZ - 1);
-        buffer[ret - 1] = '\0';
-
-        if (ret < 0 || ret >= BUFSZ) {
-                printf("Unexpected red error or overflow %d\n", ret);
-                exit(1);
-         }
-
-        ret = connect(sk, (struct sockaddr*)&name, sizeof(name));
-        if (ret) {
-                perror("Unable to connect to socket");
-                exit(1);
-        }
-
-        ret = write(sk, buffer, BUFSZ - 1);
-        if (ret < 0) {
-                perror("Unable to write");
-        }
-        if (strcmp(buffer, "PRINT") == 0) {
-                ret = read (STDIN_FILENO, buffer, BUFSZ - 1);
-                buffer[ret - 1] = '\0';
-
-                if (ret < 0 || ret >= BUFSZ) {
-                        printf("Unexpected red error or overflow %d\n", ret);
-                        exit(1);
-
-                }
-                ret = write(sk, buffer, BUFSZ - 1);
-                if (ret < 0) {
-                        perror("Unable to write");
-                }
+        perror("Too few arguments\n");
+        exit(1);
+    }
 
 
-        }
+    int sk, ret;
+    struct sockaddr_in name = {0};
 
-        return 0;
+    sk = socket(AF_INET, SOCK_STREAM, 0);
+    if (sk < 0) {
+        perror("Unable to create socket");
+        exit(1);
+    }
+
+    name.sin_family = AF_INET;
+    name.sin_port = htons(23456);
+    name.sin_addr.s_addr = inet_addr(argv[1]);
+
+    if (name.sin_addr.s_addr == INADDR_NONE) {
+
+        perror("Unable to connect to socket");
+        exit(1);
+
+
+    }
+
+    char buffer[BUFSZ-1] = {0};
+    ret = read (STDIN_FILENO, buffer, BUFSZ - 1);
+    buffer[ret - 1] = '\0';
+
+    if (ret < 0 || ret >= BUFSZ) {
+        printf("Unexpected red error or overflow %d\n", ret);
+        exit(1);
+    }
+
+    ret = connect(sk, (struct sockaddr*)&name, sizeof(name));
+    if (ret) {
+        perror("Unable to connect to socket");
+        exit(1);
+    }
+
+    ret = write(sk, buffer, BUFSZ - 1);
+    if (ret < 0) {
+        perror("Unable to write");
+    }
+    
+
+    return 0;
 }
